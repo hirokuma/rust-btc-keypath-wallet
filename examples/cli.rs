e60a774032a5c0565c1a6dd61b49189cf307723a
hirokuma <hiro9ma@gmail.com>
2026-07-18 17:05:30 +0900

    update

--- examples/cli.rs
+++ examples/cli.rs
@@ -57,7 +57,7 @@ enum Commands {
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(tracing::Level::INFO)
         .init();
     tracing::info!("bdk_wallet example");
 
